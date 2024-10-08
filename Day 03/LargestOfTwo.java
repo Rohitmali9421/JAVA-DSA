010fa5339fc2a2bc523d40992faa145d7f690f0e
Rohit <Rohitsmali9421@gmail.com>
2024-10-08 22:34:56 +0530

    day 3

---
+++ Day 03/LargestOfTwo.java
@@ -0,0 +1,12 @@
+public class LargestOfTwo {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 23;
+        if (a >= b) {
+            System.out.println("A is greater");
+        } else {
+            System.out.println("B is greater");
+        }
+    }
+}
