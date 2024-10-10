62f243733456220571ec14ce394cebe409730bfb
Rohit <Rohitsmali9421@gmail.com>
2024-10-10 23:17:06 +0530

    Day 04

---
+++ Day 04/Multiplication.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Multiplication {
+    public static int multiply(int a,int b){
+        return a*b;
+    }
+    public static void main(String[] args) {
+       System.out.println(multiply(4, 5));
+    }
+}
---
+++ Day 04/firstFunction.java
@@ -0,0 +1,9 @@
+public class firstFunction {
+
+    public static void printHello(){
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+        printHello();
+    }
+}
