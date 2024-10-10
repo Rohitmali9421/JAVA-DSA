68df1f844578ea5220fb38290fbc49d5236d037f
Rohit <Rohitsmali9421@gmail.com>
2024-10-10 23:16:56 +0530

    Day 04

---
+++ Day 04/Factorial.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Factorial {
+    public static int factorial(int a){
+        int fact=1;
+        for(int i=1;i<=a;i++){
+            fact=fact*i;
+        }
+        return fact;
+    }
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+}
