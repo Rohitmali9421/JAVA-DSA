5069d741f30ccfa0edb9b375fc9cc9eeb4859c6b
Rohit <Rohitsmali9421@gmail.com>
2025-01-04 23:38:21 +0530

    day 06

---
+++ Day 06/subarray.java
@@ -0,0 +1,19 @@
+
+public class subarray {
+    public static void sub (int arr[]) {
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                System.out.print("[");
+                for(int k=i ;k<=j;k++){
+                    System.out.print(arr[k] +" ");
+                }
+                System.out.print("] ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6};
+        sub(arr);
+    }
+}
