1abb057e8d070cd42fa77133710d29a6409c324a
Rohit <Rohitsmali9421@gmail.com>
2025-01-06 15:50:58 +0530

    day 08

---
+++ Day 08/Stock.java
@@ -0,0 +1,20 @@
+
+public class Stock {
+    public static void MaxProfit(int[] arr) {
+        int BuyPrice=Integer.MAX_VALUE;
+        int MaxProfit=0;
+        for(int i=0;i<arr.length;i++){
+            if(BuyPrice < arr[i]){
+                MaxProfit=Math.max(MaxProfit, (arr[i]-BuyPrice));
+            }else{
+                BuyPrice=arr[i];
+            }
+        }
+        System.out.println(MaxProfit);
+
+    }
+    public static void main(String[] args) {
+        int arr[]={7,1,5,3,6,5};
+        MaxProfit(arr);
+    }
+}
