b20644a0a70ea464c8cb2f8321ff9da319fd32a7
Rohit Mali <135407780+Rohitmali9421@users.noreply.github.com>
2025-01-13 19:18:42 +0530

    Update BubbleSort.java

--- Day 10/BubbleSort.java
+++ Day 10/BubbleSort.java
@@ -6,6 +6,7 @@ public class BubbleSort {
                 if(arr[j]>arr[j+1]){
                     int temp=arr[j];
                     arr[j]=arr[j+1];
+                    
                     arr[j+1]=temp;
                 }
             }
