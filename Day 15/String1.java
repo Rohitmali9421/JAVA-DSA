e87868223dddd9f83564926e8aec02104c2361f7
Rohit <Rohitsmali9421@gmail.com>
2025-01-15 23:11:11 +0530

    day 15

---
+++ Day 15/String1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class String1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Name");
+        String name=sc.next();
+        sc.nextLine();
+        System.out.println("Enter Name");
+        String name2=sc.nextLine();
+        String name3=new String("RSM");
+
+        System.out.println(name);
+        System.out.println(name2);
+        System.out.println(name3);
+    }
+}
