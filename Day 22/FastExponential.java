752c7e8f3036c629794e67a71ca6d51271ec4138
Rohit <Rohitsmali9421@gmail.com>
2025-01-24 22:10:40 +0530

    day 22

---
+++ Day 22/FastExponential.java
@@ -0,0 +1,17 @@
+
+public class FastExponential {
+    public static int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            ans[i] = ans[i >> 1] + (i & 1);
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int ans[]=countBits(5);
+
+        for(int i=0;i<ans.length;i++){
+            System.out.println(ans[i]);
+        }
+    }
+}
