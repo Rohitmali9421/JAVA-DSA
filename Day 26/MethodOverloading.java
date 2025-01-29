11c06159a519a93d10f2f9cc34e5c9b6f2d5bd46
Rohit <Rohitsmali9421@gmail.com>
2025-01-29 21:56:38 +0530

    day 26

---
+++ Day 26/MethodOverloading.java
@@ -0,0 +1,14 @@
+
+public class MethodOverloading {
+    public static void sum(int a ,int b){
+        System.out.println(a+b);
+    }
+    public static void sum(double a ,double b){
+        System.out.println(a+b);
+    }
+
+    public static void main(String[] args) {
+        sum(1,2);
+        sum(1.1,2.1);
+    }
+}
