e76c0f3b22761aa3ea77fe431b9eee74c0c45b34
Rohit <Rohitsmali9421@gmail.com>
2025-02-07 23:20:52 +0530

    day 33

---
+++ Day 33/Hashing.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Hashing {
+    public static void main(String[] args) {
+        HashMap<String , Integer> sm=new HashMap<>();
+        sm.put("A", 22);
+        sm.put("B", 23);
+        sm.put("C", 24);
+        System.out.println(sm);
+    }
+}
