9c749e495d162303f339e6b785273041c387b002
Rohit <Rohitsmali9421@gmail.com>
2025-03-14 22:45:52 +0530

    day 70

---
+++ Day 70/NextGreater.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+public class NextGreater {
+    public static void main(String[] args) {
+        int arr[]={6,8,0,1,3};
+        Stack<Integer> s=new Stack<>();
+        int nexgre[]=new int[arr.length];
+
+        for(int i = arr.length-1;i>=0;i--){
+            while(!s.isEmpty() && arr[s.peek()]<= arr[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                nexgre[i]=-1;
+            }else{
+                nexgre[i]=arr[s.peek()];
+            }
+            s.push(i);
+        }
+
+
+        for(int i=0;i<nexgre.length;i++){
+            System.out.print(nexgre[i] + " ");
+        }
+    }
+}
